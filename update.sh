91ed581a1def410d665259087df297905b424b0f
Todd Butler <toddbutler93@outlook.com.au>
2023-04-28 20:11:37 +1000

    Updated taserver.sh script with CookedPC Mount & Login server update

--- update.sh
+++ update.sh
@@ -1,12 +1,16 @@
 wget -O tribvm.zip https://github.com/JigglyJoogins/tribes-vm/archive/refs/heads/main.zip
 unzip -o tribvm.zip
 
-chmod +x tribes-vm-main/gametypes/taserver.sh
+chmod +x tribes-vm-main/taserver.sh
 chmod +x tribes-vm-main/launchservers.sh
 chmod +x tribes-vm-main/update.sh
 
 rm tribvm.zip
-rm -r gametypes
+rm -r arenasettings
+rm -r mixersettings
+rm -r pugsettings
+rm -r taserver
+
 rm -r launchservers.sh
 
 mv -f tribes-vm-main/update.sh update.new.sh
