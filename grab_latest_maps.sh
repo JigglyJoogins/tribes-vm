ae5e1d1a90c178a8dfb70cc1bb37e2dc83c7e8d9
Dodge <toddbutler93@outlook.com.au>
2023-10-20 23:58:05 +1100

    Update grab_latest_maps.sh

--- grab_latest_maps.sh
+++ grab_latest_maps.sh
@@ -5,7 +5,8 @@
 
 set -ex
 
-if [ ! -d CookedPC ] then
+if [ ! -d CookedPC ] 
+then
   docker cp taserver_pugsettings_50:/app/Tribes/TribesGame/CookedPC/ CookedPC
 
   docker stop taserver_pugsettings_50
