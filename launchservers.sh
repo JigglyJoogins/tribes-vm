36277503774c28809b1bd78289614300d6e5e386
Dodge <toddbutler93@outlook.com.au>
2023-10-20 23:16:10 +1100

    Cleanup launch server script

--- launchservers.sh
+++ launchservers.sh
@@ -2,7 +2,7 @@
 # ./taserver.sh -d cappingsettings -c CookedPC -p 6 -q "ta.dodgesdomain.com"
 
 PS3='Which server would you like to start: '
-options=("PUG Server" "Mixer Server" "2v2 Server" "Mixer Testing Server" "Map Testing Server" "OOTG Arena Server" "Arena Server" "PUB Server" "Capping Practice Server" "Exit Script")
+options=("PUG Server" "Mixer Server" "2v2 Server" "Map Testing Server" "PUB Server" "Capping Practice Server" "Exit Script")
 select opt in "${options[@]}"
 do
     case $opt in
@@ -11,24 +11,13 @@ do
             ./taserver.sh -d pugsettings -q "ta.dodgesdomain.com" -c CookedPC
             break
             ;;
-        "Arena Server")
-            echo "Starting Arena Server!"
-            ./taserver.sh -d arenasettings -p 2 -q "ta.dodgesdomain.com"
-            break
-            ;;
         "PUB Server")
             echo "Starting PUB Server!"
-            ./taserver.sh -d gamesettings -p 4
-            break
-            ;;
-        "OOTBGOTY Arena Server")
-            echo "Starting OOTBGOTY Arena Server!"
-            ./taserver.sh -d ootgarenasettings -p 6 -q "ta.dodgesdomain.com" # -g "TAMods-Server.dll"
+            ./taserver.sh -d pubsettings -p 4 -q "ta.dodgesdomain.com"
             break
             ;;
         "2v2 Server")
             echo "Starting 2v2 Server!"
-            cp -R -n pugsettings/. 2v2settings
             ./taserver.sh -d 2v2settings -p 8 -q "ta.dodgesdomain.com" -c CookedPC
             break
             ;;
@@ -37,20 +26,13 @@ do
             ./taserver.sh -d mixersettings -p 10 -q "ta.dodgesdomain.com" -c CookedPC
             break
             ;;
-        "Mixer Testing Server")
-            echo "Starting Mixer Testing Server!"
-            ./taserver.sh -d mixertestsettings -p 12 -q "ta.dodgesdomain.com" # -g "TAMods-Server.dll"
-            break
-            ;;
         "Map Testing Server")
             echo "Starting Map Testing Server!"
-            cp -R -n pugsettings/. maptest
             ./taserver.sh -d maptest -c CookedPC -p 14 -q "ta.dodgesdomain.com"
             break
             ;;
         "Capping Practice Server")
             echo "Starting Capping Practice Server!"
-            cp -R -n pugsettings/. cappingsettings
             ./taserver.sh -d cappingsettings -p 16 -q "ta.dodgesdomain.com"
             break
             ;;
